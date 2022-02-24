8b771d6b129e6bcda9a1c68f12a7502d768a65a9
Davis1908 <99963634+Davis1908@users.noreply.github.com>
2022-02-24 18:36:17 +0100

    Update iatnostrobluarancio.js

--- iatnostrobluarancio.js
+++ iatnostrobluarancio.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://davis1908.github.io/hello-world/codicegrandebluarancio.js'], function(APIConstructor, stiatExtension){
+define(['pipAPI', 'https://davis1908.github.io/hello-world/codicegrandeverde.js'], function(APIConstructor, stiatExtension){
 	
 	var API = new APIConstructor();
 		  return stiatExtension({
@@ -10,7 +10,7 @@ define(['pipAPI', 'https://davis1908.github.io/hello-world/codicegrandebluaranci
 		      height : 7 //Used to position the "Or" in the combined block.
 		    }, 
 		    media : [ //Stimuli content as PIP's media objects
-    		    	{image : 'NBYBLUARANCIO.jpg'}
+    		    	{image : 'NBYVERDE.jpg'}
 		    ], 
 		    //Stimulus css (style)
 		    css : {color:'#191970','font-size':'3em'}
