e4f8c44902a87717ad5263bbc4701f6dac4c0927
Davis1908 <99963634+Davis1908@users.noreply.github.com>
2022-03-01 19:45:28 +0100

    Update iatnostroviola.js

--- iatnostroviola.js
+++ iatnostroviola.js
@@ -29,10 +29,10 @@ define(['pipAPI', 'https://davis1908.github.io/hello-world/codicegrandeviola.js'
 				{word: 'Stranger'},
 				{word: 'Disinterested'},
 				{word: 'Trivial'},
-				{word: 'Emotionless'},
+				
 				{word: 'Extraneous'},
 				{word: 'Impersonal'},
-			        {word: 'Neutral'}
+			      
 			], 
 			//Can change color and size of the targets here.
 			css : {color:'#31b404','font-size':'3em'}
@@ -51,9 +51,9 @@ define(['pipAPI', 'https://davis1908.github.io/hello-world/codicegrandeviola.js'
 				{word: 'Relevant'},
 				{word: 'Meaningful'},
 				{word: 'Loved'},
-				{word: 'Bonded'},
+			
 				{word: 'Intimate'},
-			  {word: 'Dear to me'}
+			
 			], 
 			//Can change color and size of the targets here.
 			css : {color:'#31b404','font-size':'3em'}
