4b352b63e2ad0942dea4b5c8e8c033ae8830dc4a
Davis1908 <99963634+Davis1908@users.noreply.github.com>
2022-03-08 10:21:29 +0100

    Update codicegrande.js

--- codicegrande.js
+++ codicegrande.js
@@ -90,9 +90,9 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 					instHTML : '', 
 					block : 2, 
 					miniBlocks : 1, 
-					singleAttTrials : 9, 
-					sharedAttTrials : 9, 
-					categoryTrials : 0
+					singleAttTrials : 6, 
+					sharedAttTrials : 6, 
+					categoryTrials : 6
 				}, 
 				{ 
 					instHTML : '', 
