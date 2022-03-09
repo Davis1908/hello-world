69e628e818113cdb04659cf5846351f249017454
Davis1908 <99963634+Davis1908@users.noreply.github.com>
2022-03-09 11:27:38 +0100

    Update iatnostro.js

--- iatnostro.js
+++ iatnostro.js
@@ -10,7 +10,7 @@ define(['pipAPI', 'https://davis1908.github.io/hello-world/codicegrande.js'], fu
 		      height : 7 //Used to position the "Or" in the combined block.
 		    }, 
 		    media : [ //Stimuli content as PIP's media objects
-    		    	{image : 's000.jpg'}
+    		    	{image : 's0000.jpg'}
 		    ], 
 		    //Stimulus css (style)
 		    css : {color:'#191970','font-size':'3em'}
