eaaf80752ee4391f37129a6e22ed8926122f0bbb
Davis1908 <99963634+Davis1908@users.noreply.github.com>
2022-03-18 18:44:10 +0100

    Update iatnostro06.js

--- iatnostro06.js
+++ iatnostro06.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://davis1908.github.io/hello-world/codicegrande05.js'], function(APIConstructor, stiatExtension){
+define(['pipAPI', 'https://davis1908.github.io/hello-world/codicegrande06.js'], function(APIConstructor, stiatExtension){
 	
 	var API = new APIConstructor();
 		  return stiatExtension({
